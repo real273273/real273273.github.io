de0172d739616627b0c0a49ab84db82ec952836f
real273273 <rararara0352@mail.ru>
2018-11-10 14:07:42 +0300

    Update common.js

--- js/common.js
+++ js/common.js
@@ -1,4 +1,3 @@
-
 $(document).ready(function() {
   $('.slider-for').slick({
   slidesToShow: 1,
@@ -6,14 +5,32 @@ $(document).ready(function() {
   arrows: false,
   fade: true,
   dots: true,
-  asNavFor: '.slider-nav'
+  asNavFor: '.slider-nav',
+  responsive: [
+    {
+      breakpoint: 768,
+//сообщает, при какой ширине экрана нужно включать настройки
+      settings: {
+        dots: false,
+      }
+    }
+  ]
 });
 $('.slider-nav').slick({
   slidesToShow: 5,
   slidesToScroll: 1,
   asNavFor: '.slider-for',
-  
-  
-  focusOnSelect: true
+  focusOnSelect: true,
+  responsive: [
+    {
+      breakpoint: 768,
+//сообщает, при какой ширине экрана нужно включать настройки
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        infinite: true,
+      }
+    }
+]
 });
 });
