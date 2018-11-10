8ebac15751ebafe0755b1c6c31d30cc8d6848861
real273273 <rararara0352@mail.ru>
2018-11-10 12:32:50 +0300

    Update common.js

--- js/common.js
+++ js/common.js
@@ -1,1 +1,19 @@
 
+$(document).ready(function() {
+  $('.slider-for').slick({
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  arrows: false,
+  fade: true,
+  dots: true,
+  asNavFor: '.slider-nav'
+});
+$('.slider-nav').slick({
+  slidesToShow: 5,
+  slidesToScroll: 1,
+  asNavFor: '.slider-for',
+  
+  
+  focusOnSelect: true
+});
+});
