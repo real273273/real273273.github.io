37af6896816ad494144b6001a96ae6ffe785c19f
real273273 <rararara0352@mail.ru>
2018-11-17 14:26:44 +0300

    Update common.js

--- js/common.js
+++ js/common.js
@@ -45,7 +45,17 @@ $('.popup').magnificPopup({
   mainClass: 'mfp-fade'
  }); 
 
-
-
-
+ $(document).ready(function(){
+    $("a[href*=#]").on("click", function(e){
+        var anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: $(anchor.attr('href')).offset().top
+        }, 777);
+        e.preventDefault();
+        return false;
+    });
 });
+ 
+
+
+  });
