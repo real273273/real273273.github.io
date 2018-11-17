4f5d2f0a8b266ee30d656f0a10a59476324c4d46
real273273 <rararara0352@mail.ru>
2018-11-17 22:01:54 +0300

    Update common.js

--- js/common.js
+++ js/common.js
@@ -45,17 +45,19 @@ $('.popup').magnificPopup({
   mainClass: 'mfp-fade'
  }); 
 
- $(document).ready(function(){
-    $("a[href*=#]").on("click", function(e){
-        var anchor = $(this);
-        $('html, body').stop().animate({
-            scrollTop: $(anchor.attr('href')).offset().top
-        }, 777);
-        e.preventDefault();
-        return false;
-    });
-});
- 
+$("#menu").on("click","a", function (event) {
+    //отменяем стандартную обработку нажатия по ссылке
+    event.preventDefault();
 
+    //забираем идентификатор бока с атрибута href
+    var id  = $(this).attr('href'),
 
+    //узнаем высоту от начала страницы до блока на который ссылается якорь
+      top = $(id).offset().top;
+    
+    //анимируем переход на расстояние - top за 1500 мс
+    $('body,html').animate({scrollTop: top}, 777);
   });
+
+
+});
